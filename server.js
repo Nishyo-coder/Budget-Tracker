cede787f2ad19648d75e3514269000b90e051d8e
Nish Mitchell <shanishamit@gmail.com>
2021-08-13 17:16:42 -0400

    moving connection

--- server.js
+++ server.js
@@ -13,14 +13,15 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
        useUnifiedTopology: true,
        useCreateIndex: true,
        useFindAndModify: false
 });
+app.use(express.static("public"));
+
+
 
 // routes
 app.use(require("./routes/api.js"));
