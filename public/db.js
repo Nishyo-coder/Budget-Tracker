66588322cf1414e3c40d3602556dc03dfd4ec40b
Nish Mitchell <shanishamit@gmail.com>
2021-08-13 00:25:56 -0400

    adding the objectstore to the code

--- public/db.js
+++ public/db.js
@@ -3,24 +3,17 @@
 // THEN that will be shown on the page, and added to their transaction history when their connection is back online.
 
 let db;
-// let budgetVersion;
-
 // Create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
 //Onupgradeneeded will run if we need to create the database or upgrade it
 request.onupgradeneeded = function (e) {
     console.log('Upgrade needed in IndexDB');
-  
-    const { oldVersion } = e;
-    const newVersion = e.newVersion || db.version;
-  
-    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
-  
     db = e.target.result;
-  
     if (db.objectStoreNames.length === 0) {
       db.createObjectStore('BudgetStore', { autoIncrement: true });
+      const Budget = db.createObjectStore("BudgetStorage", {keyPath: "listID"});
+      Budget.createIndex("statusIndex", "status"); 
     }
   };
   
@@ -34,11 +27,29 @@ request.onupgradeneeded = function (e) {
     // Open a transaction on the Budget db
         // const objectStore = db.createObjectStore("toDoList");
 
-    const transaction = db.transaction(['BudgetStore'], 'readwrite');
-    objectStore.createIndex("BudgetStore", "BudgetStore");
+    let transaction = db.transaction(['BudgetStorage'], 'readwrite');
+    const Budget = transaction.objectStore("BudgetStorage", {keyPath: "listID"});
+    const statusIndex = Budget.index("statusIndex");
+  
+    // Adds data to our objectStore
+    Budget.add({ listID: "1", status: "pending" });
+    Budget.add({ listID: "2", status: "pending" });
+    Budget.add({ listID: "3", status: "pending" });
+    Budget.add({ listID: "4", status: "pending" });
 
+    const getRequest = Budget.get("1");
+    getRequest.onsuccess = () => {
+    console.log(getRequest.result);
+        };
+
+     // Return an item by index
+    const getRequestIdx = statusIndex.getAll("complete");
+    getRequestIdx.onsuccess = () => {
+    console.log(getRequestIdx.result); 
+    };
+    // objectStore.createIndex("BudgetStore", "BudgetStore");
     // access your Budget object
-    const store = transaction.objectStore('BudgetStore');
+    const store = transaction.objectStore('BudgetStorage');
     // Get all records from store and set to a variable
     const getAll = store.getAll();
   
@@ -59,13 +70,9 @@ request.onupgradeneeded = function (e) {
             // If our returned response is not empty
             if (res.length !== 0) {
               // Open another transaction to BudgetStore with the ability to read and write
-              transaction = db.transaction(['BudgetStore'], 'readwrite');
-  
-              // Assign the current store to a variable
-              const currentStore = transaction.objectStore('BudgetStore');
-  
+              transaction = db.transaction(['BudgetStorage'], 'readwrite');
               // Clear existing entries because our bulk add was successful
-              currentStore.clear();
+              Store.clear();
               console.log('Clearing store 🧹');
             }
           });
@@ -87,10 +94,9 @@ request.onupgradeneeded = function (e) {
   const saveRecord = (record) => {
     console.log('Save record invoked');
     // // Create a transaction on the BudgetStore db with readwrite access
-    // const transaction = db.transaction(['BudgetStore'], 'readwrite');
-  
+    const transaction = db.transaction(["BudgetStore"], 'readwrite');
     // // Access your BudgetStore object store
-    // const store = transaction.objectStore('BudgetStore');
+    const store = transaction.objectStore("BudgetStore");
   
     // Add record to your store with add method.
     store.add(record);
