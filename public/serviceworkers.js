43593e13a87f761fe1a19a33876b06c772944c4e
Nish Mitchell <shanishamit@gmail.com>
2021-08-13 19:16:45 -0400

    added manifest from activity 8 and updated service workers

---
+++ public/serviceworkers.js
@@ -0,0 +1,48 @@
+const expectedCaches = ['static-v2'];
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "./index.html",
+    "./db.js",
+    "./index.js",
+    "./styles.css",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png"
+  ];
+  
+  self.addEventListener('install', event => {
+    console.log();
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log('V2 installing…');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  });
+  
+  self.addEventListener('activate', event => {
+    // delete any caches that aren't in expectedCaches
+    // which will get rid of static-v1
+    event.waitUntil(
+      caches.keys().then(keys => Promise.all(
+        keys.map(key => {
+          if (!expectedCaches.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )).then(() => {
+        console.log('V2 now ready to handle fetches!');
+      })
+    );
+  });
+  
+  
+  self.addEventListener('fetch', event => {
+    if (event.request.url.includes('/api/')) {
+      console.log('[Service Worker] Fetch(data)', event.request.url);
+      event.respondWith( 
+        caches.open(DATA_CACHE_NAME).then ||
+        fetch(event.request)
+      );
+      }});
