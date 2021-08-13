245ae8f85fc757e3bf7128bfbb39a39723cc69a6
Nish Mitchell <shanishamit@gmail.com>
2021-08-13 12:01:32 -0400

    reorganized the code to get the post api

--- public/db.js
+++ public/db.js
@@ -11,68 +11,17 @@ request.onupgradeneeded = function (e) {
     console.log('Upgrade needed in IndexDB');
     db = e.target.result;
     if (db.objectStoreNames.length === 0) {
-      db.createObjectStore('BudgetStorage', { autoIncrement: true });
-      const Budget = db.createObjectStore("BudgetStorage", {keyPath: "listID"});
-      Budget.createIndex("statusIndex", "status"); 
-    }
-  };
-  
-  request.onerror = function (e) {
-    console.log(`Woops! ${e.target.errorCode}`);
-  };
-  
-  function checkDatabase() {
-    console.log('check db invoked');
-    let transaction = db.transaction(['BudgetStorage'], 'readwrite');
-    const Budget = transaction.objectStore("BudgetStorage", {keyPath: "listID"});
-    const statusIndex = Budget.index("statusIndex");
-  
+    let Budget = db.createObjectStore('BudgetStorage', {keyPath: "listID"},{ autoIncrement: true });
+    //   const Budget = db.createObjectStore("BudgetStorage", );
+    Budget.createIndex("statusIndex", "status"); 
+
     // Adds data to our objectStore
     Budget.add({ listID: "1", status: "pending" });
     Budget.add({ listID: "2", status: "pending" });
     Budget.add({ listID: "3", status: "pending" });
     Budget.add({ listID: "4", status: "pending" });
-
-     // Return an item by index
-    const getRequestIdx = statusIndex.getAll("complete");
-    getRequestIdx.onsuccess = () => {
-    console.log(getRequestIdx.result); 
-    };
-    // objectStore.createIndex("BudgetStore", "BudgetStore");
-    // access your Budget object
-    const store = transaction.objectStore('BudgetStorage');
-    // Get all records from store and set to a variable
-    const getAll = store.getAll();
-  
-    // If the request was successful
-    getAll.onsuccess = function () {
-        console.log(getAll)
-      // If there are items in the store, we need to bulk add them when we are back online
-      if (getAll.result.length > 0) {
-        fetch('/api/transaction/bulk', {
-          method: 'POST',
-          body: JSON.stringify(getAll.result),
-          headers: {
-            Accept: 'application/json, text/plain, */*',
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((response) => response.json())
-          .then((res) => {
-            // If our returned response is not empty
-            if (res.length !== 0) {
-              // Open another transaction to BudgetStore with the ability to read and write
-              transaction = db.transaction(['BudgetStorage'], 'readwrite');
-              // Clear existing entries because our bulk add was successful
-            //   const store = transaction.objectStore('BudgetStorage');
-              store.clear();
-              console.log('Clearing store 🧹');
-            }
-          });
-      }
-    };
-  }
-  
+    }
+  };
   request.onsuccess = function (e) {
     console.log('success');
     db = e.target.result;
@@ -83,17 +32,69 @@ request.onupgradeneeded = function (e) {
       checkDatabase();
     }
   };
-  
+
+  request.onerror = function (e) {
+    console.log(`Woops! ${e.target.errorCode}`);
+  };
+
   const saveRecord = (record) => {
     console.log('Save record invoked');
     // // Create a transaction on the BudgetStore db with readwrite access
     const transaction = db.transaction(["BudgetStore"], 'readwrite');
     // // Access your BudgetStore object store
     const store = transaction.objectStore("BudgetStorage");
-  
     // Add record to your store with add method.
     store.add(record);
-  };
+}
+
+function checkDatabase() {
+console.log('check db invoked');
+    const transaction = db.transaction(['BudgetStorage'], 'readwrite');
+    const store = transaction.objectStore("BudgetStorage");
+    const getAll = store.getAll();
+    const Budget = transaction.objectStore("BudgetStorage");
+        const statusIndex = Budget.index("statusIndex");
+        const getRequestIdx = statusIndex.getAll("complete");
+        getRequestIdx.onsuccess = () => {
+        console.log(getRequestIdx.result); 
+
+    getAll.onsuccess = function () {
+        console.log(getAll)
+    }
+}
+       
+// Get all records from store and set to a variable
+  // If there are items in the store, we need to bulk add them when we are back online
+  if (getAll.result.length > 0) {
+    fetch('/api/transaction/bulk', {
+      method: 'POST',
+      body: JSON.stringify(getAll.result),
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+    })
+
+      .then((response) => response.json())
+      .then((res) => {
+        // If our returned response is not empty
+        if (res.length !== 0) {
+          // Open another transaction to BudgetStore with the ability to read and write
+          transaction = db.transaction(['BudgetStorage'], 'readwrite');
+          // Clear existing entries because our bulk add was successful
+        //   const store = transaction.objectStore('BudgetStorage');
+          store.clear();
+          console.log('Clearing store 🧹');
+        }
+    
+      });
+    // const Budget = transaction.objectStore("BudgetStorage", {keyPath: "listID"});  
+  }
+ } // objectStore.createIndex("BudgetStore", "BudgetStore");
+    // access your Budget object
+  
+    // If the request was successful
+  
   
   // Listen for app coming back online
   window.addEventListener('online', checkDatabase);
